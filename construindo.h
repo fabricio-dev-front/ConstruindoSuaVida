8c9a8c9cdaf3a3eb718aec69d38609c27d15f843
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2024-09-16 20:15:28 -0300

    feat:adjustes

---
+++ construindo.h
@@ -0,0 +1,40 @@
+#ifndef MET_H
+#define MET_H
+
+#include <stdbool.h>
+
+struct Usuario{
+    char nome[50];
+    char email[50];
+    char senha[20];
+};
+
+struct Produto{
+    int codigo;
+    char nome[50];
+    float preco;
+}; struct ProdutoNode* carregarProdutos();
+
+struct UsuarioNode{
+    struct Usuario data;
+    struct UsuarioNode *next;
+};
+
+struct ProdutoNode{
+    struct Produto data;
+    struct ProdutoNode *next;
+};
+
+bool validarEmail(const char *email);
+struct ProdutoNode* carregarProdutos();
+void cadastrarUsuario(struct UsuarioNode **head, int *totalUsuarios);
+void fazerLogin(struct UsuarioNode *head, bool *logado);
+void exibirTabelaDeProdutos(struct ProdutoNode *head);
+void adicionarProduto(struct ProdutoNode **head, int *totalProdutos);
+void removerProduto(struct ProdutoNode **head, int *totalProdutos);
+void buscarProdutoPorCodigo(struct ProdutoNode *head); 
+void salvarProdutos(struct ProdutoNode *head);
+
+void ordenarProdutos();
+
+#endif 
---
+++ main.c
@@ -0,0 +1,82 @@
+#include "construindo.h"
+#include <stdio.h>
+
+#define MAX_USUARIOS 10
+
+int main(){
+    struct UsuarioNode *usuarios = NULL;
+    int totalUsuarios = 0;
+    bool logado = false;
+
+    struct ProdutoNode *produtos = carregarProdutos();
+    int totalProdutos = 0;
+
+    int opcao;
+    do{
+        printf("\n=== MENU DE USUARIO ===\n");
+        printf("1. Cadastrar usuario\n");
+        printf("2. Fazer login\n");
+        printf("3. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao){
+            case 1:
+                cadastrarUsuario(&usuarios, &totalUsuarios);
+                break;
+            case 2:
+                fazerLogin(usuarios, &logado);
+                break;
+            case 3:
+                printf("Saindo...\n");
+                return 0;
+        }
+    } while(!logado);
+
+    do{
+        printf("\n=== MENU DE PRODUTOS ===\n");
+        printf("1. Exibir tabela de produtos\n");
+        printf("2. Adicionar produto\n");
+        printf("3. Remover produto\n");
+        printf("4. Buscar produto por codigo\n");
+        printf("5. Salvar produtos\n");
+        printf("6. Ordenar produtos\n");
+        printf("7. Sair\n");
+        printf("---------------------------------------------------------------\n");
+        printf("ANTES DE SAIR, ORDENE O ARQUIVO NA OPCAO 6 (Ordenar produtos)\n");
+        printf("---------------------------------------------------------------\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao){
+            case 1:
+                exibirTabelaDeProdutos(produtos);
+                break;
+            case 2:
+                adicionarProduto(&produtos, &totalProdutos);
+                break;
+            case 3:
+                removerProduto(&produtos, &totalProdutos);
+                break;
+            case 4:
+                buscarProdutoPorCodigo(produtos);
+                break;
+            case 5:
+                salvarProdutos(produtos);
+                printf("Produtos salvos com sucesso!\n");
+                break; 
+            case 6:
+                ordenarProdutos(&produtos);
+                printf("Produtos ordenados com sucesso!\n");
+                break;    
+            case 7:
+                printf("saindo...\n");
+                logado = 0;
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");    
+        }
+    } while(logado);
+
+    return 0;
+}
